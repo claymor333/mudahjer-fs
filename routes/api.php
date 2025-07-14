1b2834cc0c7df2b7932c93e2590f122ed2ab22b8
Muhammad Zaim <linkguardian878@gmail.com>
2025-07-14 16:56:56 +0800

    comments

--- routes/api.php
+++ routes/api.php
@@ -5,10 +5,18 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 
 
-/// {url}/api/register - logins and returns bearer token
+/// {url}/api/register - register and returns bearer token
+/// params -----
+/// name, string max:255
+/// email, string email max:255 unique:users
+/// password, min:8
+/// password_confirmation
 Route::post('/register', [AuthController::class, 'register']);
 
 /// {url}/api/login - logins and returns bearer token
+/// params -----
+/// name, string max:255
+/// email
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::get('/user', function (Request $request) {
