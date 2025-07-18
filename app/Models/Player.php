69b7e2a7f7d8f2d27cf71d5d2ee8114fa41396f5
Nasrulhaq Hidayat Abdul Latiff <nasrulhaqhidayat@gmail.com>
2025-07-19 03:06:41 +0800

    completed

--- app/Models/Player.php
+++ app/Models/Player.php
@@ -7,4 +7,22 @@ use Illuminate\Database\Eloquent\Model;
 class Player extends Model
 {
     //
+    protected $fillable = [
+        'user_id',
+        'username',
+        'avatar',
+        'level',
+        'exp',
+    ];
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function lessons()
+    {
+        return $this->belongsToMany(Lesson::class, 'players_lessons', 'player_id', 'lesson_id')
+            ->withPivot('completed', 'completed_at')
+            ->withTimestamps();
+    }
 }
