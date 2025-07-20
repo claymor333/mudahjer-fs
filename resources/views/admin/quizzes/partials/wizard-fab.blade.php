18264abc2940f9d56e7e6efae398de24cbd628f9
Muhammad Zaim <linkguardian878@gmail.com>
2025-07-20 23:57:10 +0800

    feat: determine item addition dynamically

--- resources/views/admin/quizzes/partials/wizard-fab.blade.php
+++ resources/views/admin/quizzes/partials/wizard-fab.blade.php
@@ -35,14 +35,14 @@
 		<div class="tooltip-content">
 			<div class="flex-col">
 				<p>
-					Add Question
+					Add Item
 				</p>
 				<div class="kbd-inst mt-2">
 					<kbd class="kbd kbd-sm">CTRL</kbd> + <kbd class="kbd kbd-sm">E</kbd>
 				</div>
 			</div>
 		</div>
-		<button type="button" class="btn btn-circle btn-lg btn-primary fab" onclick="addQuestion()">
+		<button type="button" class="btn btn-circle btn-lg btn-primary fab" onclick="addQuestionOrNote()">
 			<svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
 				<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
 			</svg>
