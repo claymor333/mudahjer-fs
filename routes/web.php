c9d0cd50e11e13c2fd53625d89382fe1a1b514de
Muhammad Zaim <linkguardian878@gmail.com>
2025-07-22 02:13:32 +0800

    feat: blade directives

--- routes/web.php
+++ routes/web.php
@@ -2,20 +2,23 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Admin\QuizController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\Player\QuizController as PlayerQuizController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/player/quizzes', [PlayerQuizController::class, 'index'])->name('player.quizzes.index');
+    Route::get('/player/quiz/play/{id}', [PlayerQuizController::class, 'show'])->name('player.quizzes.play');
 });
 
 Route::middleware(['auth', 'role:admin'])->prefix('admin')->name('admin.')->group(function () {
