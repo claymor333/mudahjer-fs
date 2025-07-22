915e051bd95ddebba276430efb6ebe6b0ae4fbf0
Muhammad Zaim <linkguardian878@gmail.com>
2025-07-23 00:22:48 +0800

    feat: submission api

--- routes/api.php
+++ routes/api.php
@@ -28,7 +28,8 @@ Route::group(['middleware' => 'auth:sanctum'], function () {
     Route::get('/lessons/{user_id}', [QuizController::class, 'getLessons']);
     Route::get('/quizzes/{lesson_id}', [QuizController::class,'getQuizzes']);
     Route::get('/questions/{quiz_id}', [QuizController::class,'getQuestions']);  // amik semua dalam quiz
-    Route::get('/choices/{question_id}', [QuizController::class,'getQuizzes']);
+
+    Route::post('lessons/{lesson_id}/questions/{quiz_id}/submit', [QuizController::class,'submitQuiz']);
 });
 // Get Quizzes for a specific lesson
 // Get Questions for a specific quiz
